83f04a601ab7d9ae0ad6c46d34ac826ba427600a
amit1212karki <amitkarki89@gmail.com>
2024-11-09 23:41:04 +0545

    first push

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+
+        return view('products.index',compact('products'))
+            ->with('i',(request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=> 'required',
+            'details'=>'required',
+        ]);
+        Product::create($request->all());
+
+        return redirect()->route('products.index')
+        ->with('success','Product created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $products)
+    {
+        return view('products.show',compact('products'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $products)
+    {
+        return view('products.edit',compact('products'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $products)
+    {
+        $request->validate([
+            'name' => 'required',
+            'details' => 'required',
+        ]);
+
+        $products->update($request->all());
+        
+        return redirect()->route('products.index')
+                        ->with('success','Products update successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $products)
+    {
+        $products->delete();
+
+        return redirect()->route('products.index')
+                        ->with('success','Products deleted successfully');
+    }
+}
